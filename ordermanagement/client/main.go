0099a9b6fd37057385cb8daac2eb9164b44b56c4
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-03-15 17:56:18 +0100

    client side stream interceptor

--- ordermanagement/client/main.go
+++ ordermanagement/client/main.go
@@ -15,6 +15,10 @@ import (
 
 const address = "localhost:50051"
 
+type wrappedStream struct {
+	grpc.ClientStream
+}
+
 // Client-side Unary Interceptor
 func orderUnaryClientInterceptor(ctx context.Context, method string, req, res interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	// Preprocessor phase has access to the RPC request prior to sending it out to the server
@@ -29,9 +33,38 @@ func orderUnaryClientInterceptor(ctx context.Context, method string, req, res in
 	return err
 }
 
+//=======================================================
+
+// Client-side Stream Interceptor
+func clientStreamInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	log.Println("======= [Client Interceptor] ", method)
+	s, err := streamer(ctx, desc, cc, method, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return newWrappedStream(s), nil
+}
+
+func (w *wrappedStream) RecvMsg(m interface{}) error {
+	log.Printf("======== [Client Stream Interceptor]"+"Receive a message (Type :%T) at %v", m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.RecvMsg(m)
+}
+
+func (w *wrappedStream) SendMsg(m interface{}) error {
+	log.Printf("======= [Client Stream Interceptor] "+"Send a message (Type: %T) at %v", m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.SendMsg(m)
+}
+
+func newWrappedStream(s grpc.ClientStream) grpc.ClientStream {
+	return &wrappedStream{s}
+}
+
+//======================================================= main function
+
 func main() {
 	// set up connection to the server
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithUnaryInterceptor(orderUnaryClientInterceptor))
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithUnaryInterceptor(orderUnaryClientInterceptor),
+		grpc.WithStreamInterceptor(clientStreamInterceptor))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
